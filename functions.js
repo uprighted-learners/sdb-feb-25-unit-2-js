ab39eb840244c65f7dfcfa4608945c1f03d8fbf0
Paul Niemczyk <mrpndev@gmail.com>
2025-03-06 19:23:03 -0500

    Functions

--- functions.js
+++ functions.js
@@ -136,7 +136,7 @@ let stringConcatenator = (str1, str2) => {
 	} else {
 		return "This is for strings dummy";
 	}
-}
+};
 
 console.log(stringConcatenator("1", 2));
 
@@ -145,25 +145,49 @@ console.log(stringConcatenator("1", 2));
     * an anonymous function that gets immediately invoked
 */
 
-(function() {
-    console.log("This is an IFFE")
-})()
+(function () {
+	console.log("This is an IFFE");
+})();
 
-let word = " Hello";
-console.log ("hello")
+/*
 
+ ? FizzBuzz Challenge!
+  Create a function that takes a parameters called start and stop.
+  Loop over numbers from start to stop
+  If the number is divisible by 3, print "Fizz"
+  If the number is divisible by 5, print "Buzz"
+  If the number is divisible by 3 and 5, print "Fizz Buzz"
+  If the number is not divisible, print the number only
+  Invoke your function to make sure it works.
+  SPICEY MODE: try to return the numbers instead. What's going on?
+*/
 
-let firstletter ="h";
-console.log ("h");
+/* 
+    ? Reverse Words in a String
+    * write a function that takes string as ain input
+    * return a reverse version of that string
+    * ex: Paul should return luaP
+    * hint: brainstorm the steps required to accomplish this
+*/
 
+/* 
+    ? Pythagoream Theorem Challenge
+    * right triangle has three sides
+    * the longest side c is calculated by taking the sum of:
+    * a**2 and b**2
+    * the formula is as follows c = sqrt(a**2 + b**2)
+    * Create a function expression that takes two triangles sides
+    * return the hypotenuse (c side)
+    * hint: Math object has a .sqrt() method
+*/
 
- 
-let str = "hello";
-let restOfWord = str.slice(1);
-console.log (restOfWord);
-
-let firsLetter = "h".toUpperCase;
-console.log (firstletter +restOfWord);
-
-console.log( restOfWord);
-console.log (firstletter + restOfWord);
+/* 
+    ? Quadratic Formula Challenge
+    * Create a function that will give you a result of a quadratic formula based on all of the necessary factors
+    * Check if the data type is Number. If not, return, "please use whole numbers only"
+    * Please return the result but also the value of the top and bottom separately
+    * make sure that the numbers are whole numbers so discriminant is positive
+    * use Math interface
+    
+    -b+-sqrt(b**2 -4ac)/2a
+*/
